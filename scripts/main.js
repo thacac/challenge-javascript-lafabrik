fc1173e451df924f58dcef3b91443840da9b3185
thacac <cedric@thecacproject.com>
2020-12-22 11:25:34 +0100

    first commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,20 @@
 /**
  * @todo Point d'entrée pour faire vos exercices...
  */
+
+const searchField = document.querySelector('#search')
+searchField.addEventListener('change', search)
+
 window.onload = init
 
 function init() {
     console.log("Coucou ! C'est ici que l'aventure commence ! Bon courage :-)")
+}
+
+
+function fetchApi(search)
+{
+    
+    
+    fetch('')
 }
